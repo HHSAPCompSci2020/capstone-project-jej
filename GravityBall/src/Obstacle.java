5b4ff6e3b003c91eb589dbaaf0d7f57821ce334f
jisiyi2005 <70030301+jisiyi2005@users.noreply.github.com>
2021-05-09 21:40:05 -0700

    basic stuff for obstacle

--- GravityBall/src/Obstacle.java
+++ GravityBall/src/Obstacle.java
@@ -1,4 +1,28 @@
 import processing.core.PApplet;
 public class Obstacle {
+	private int x;
+	private int y;	
+	public Obstacle()
+	{
+		x = 0;
+		y = 0;
+	}
+	public Obstacle(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public int getY()
+	{
+		return y;
+	}
+	
 
 }
