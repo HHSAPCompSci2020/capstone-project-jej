b3e177004b5def379e77f4dafe715ccb26c212f4
Jensen Guo <jguo407@student.fuhsd.org>
2021-05-11 10:28:12 -0700

    Surface Switching Implemented

--- GravityBall/src/ScreenSwitcher.java
+++ GravityBall/src/ScreenSwitcher.java
@@ -5,8 +5,8 @@
  *
  */
 public interface ScreenSwitcher {
-	public static final int SCREEN1 = 0;
-	public static final int SCREEN2 = 1;
+	public static final int MENU = 0;
+	public static final int GAME = 1;
 	
 	public void switchScreen(int i);
 }
