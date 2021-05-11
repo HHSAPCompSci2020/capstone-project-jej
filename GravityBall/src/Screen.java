4871f4a575c42eecfddc2017c861f47f7705e154
Jensen Guo <jguo407@student.fuhsd.org>
2021-05-11 09:59:52 -0700

    Screen.java

---
+++ GravityBall/src/Screen.java
@@ -0,0 +1,42 @@
+	
+
+
+/**
+ * Abstract superclass of screens
+ * @author APCS 2021 Demos
+ *
+ */
+public abstract class Screen {
+
+	public final int DRAWING_WIDTH, DRAWING_HEIGHT;
+	
+	public Screen(int width, int height) {
+		this.DRAWING_WIDTH = width;
+		this.DRAWING_HEIGHT = height;
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mousePressed() {
+		
+	}
+	
+	public void mouseMoved() {
+		
+	}
+	
+	public void mouseDragged() {
+		
+	}
+	
+	public void mouseReleased() {
+		
+	}
+}
+
