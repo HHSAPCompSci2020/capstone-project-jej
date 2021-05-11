87bc66e4fdb5e0557d120f8519dbf2ac20138264
Edward Min <emin572@student.fuhsd.org>
2021-05-11 10:32:40 -0700

    Added Mutators/Accessors

--- GravityBall/src/Player.java
+++ GravityBall/src/Player.java
@@ -6,5 +6,47 @@ import processing.core.PApplet;
  *
  */
 public class Player {
-
+	private double xPos, yPos;
+	private double xVel, yVel;
+	
+	public Player() {
+		xPos = 0;
+		yPos = 0;
+		xVel = 0;
+		yVel = 0;
+	}
+	
+	public Player(double x, double y) {
+		xPos = x;
+		yPos = y;
+		xVel = 0;
+		yVel = 0;
+	}
+	
+	public void setCoords(double x, double y) {
+		xPos = x;
+		yPos = y;
+	}
+	
+	public double getX() {
+		return xPos;
+	}
+	
+	public double getY() {
+		return yPos;
+	}
+	
+	public void setVelRectangular(double xv, double yv) {
+		xVel = xv;
+		yVel = yv;
+	}
+	
+	public void setVelPolar(double v, double theta) {
+		setVelRectangular(v * Math.cos(theta), v * Math.sin(theta));
+	}
+	
+	public void accelerate (double ax, double ay) {
+		xVel += ax;
+		yVel += ay;
+	}
 }
