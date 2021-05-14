524ad32bb6a36b4861840a08d8f6d3f857509166
Jensen Guo <jguo407@student.fuhsd.org>
2021-05-14 09:29:42 -0700

    Level.java added

--- GravityBall/src/Level.java
+++ GravityBall/src/Level.java
@@ -1,14 +1,38 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
- * The level class, serves as a template
- * @author jensen
+ * The level class
+ * @author Jensen
  *
  */
 public class Level {
-	public Level() {
-		
+	private ArrayList<Obstacle> obstacles;
+	private int level;
+	Point start;
+	Point end;
+	//the int passed in is the level number
+	public Level(int level) {
+		this.level = level;
+		setup();
+	}
+	
+	public void setup() {
+		obstacles = new ArrayList<Obstacle>();
+		if(level == 1) {
+			start = new Point(100, 100);
+			end = new Point(500, 500);
+			//add obstacles
+			obstacles.add(new Obstacle());
+		}
+		else {
+			System.out.println("ERROR: Level " + level + " wasn't found or doesn't exist");
+		}
 	}
 	
 	public void draw() {
-		
+		for(Obstacle o: obstacles) {
+			o.draw();
+		}
 	}
 }
